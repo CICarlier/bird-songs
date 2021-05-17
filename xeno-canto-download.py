b5b13f1913ac9e8e777c72c14cc0bb62fecdeb86
CICarlier <35584629+CICarlier@users.noreply.github.com>
2021-05-16 21:44:28 -0400

    get all features and recordings for canada and the us

--- xeno-canto-download.py
+++ xeno-canto-download.py
@@ -18,26 +18,26 @@ else:
 
 # API url must include query parameters. See https://www.xeno-canto.org/help/search
 # Query takes all birds from Michigan with quality A or B, length <= to 120 seconds.
-urls = ["https://www.xeno-canto.org/api/2/recordings?query=loc:michigan+q_gt:C+len_lt:120",
-        "https://www.xeno-canto.org/api/2/recordings?query=loc:michigan+q_gt:C+len_lt:120&page=2",
-        "https://www.xeno-canto.org/api/2/recordings?query=loc:wisconsin+q_gt:C+len_lt:120",
-        "https://www.xeno-canto.org/api/2/recordings?query=loc:ohio+q_gt:C+len_lt:120",
-        "https://www.xeno-canto.org/api/2/recordings?query=loc:illinois+q_gt:C+len_lt:120",
-        "https://www.xeno-canto.org/api/2/recordings?query=loc:ontario+q_gt:C+len_lt:120"]
+base_urls = ['https://www.xeno-canto.org/api/2/recordings?query=cnt:united_states+q_gt:C+len_lt:120',
+             'https://www.xeno-canto.org/api/2/recordings?query=cnt:canada+q_gt:C+len_lt:120']
 
-#TODO: change code to download features only first, including urls, and THEN download files. MUCH FASTER!
-for url in urls:
+for url in base_urls:
     print(url)
     r = requests.get(url, allow_redirects=True)
 
-    for result in r.json()['recordings']:
-        # Download audio file to audio folder
-        f = requests.get(f"http:{result['file']}", allow_redirects=True)
-        open(f"audio/{result['id']}.mp3", 'wb').write(f.content)
+    pages = r.json()['numPages']
+    for page in range(1, pages):
+        print(f'page {page}')
+        page_url = f'{url}&page={page}'
+        r_page = requests.get(url, allow_redirects=True)
 
-        # Download features to csv for classification
-        fields = [result['id'], result['gen'], result['sp'], result['ssp'], result['en'], result['cnt'], result['loc'],
-                  result['type'], result['q'], result['length'], result['bird-seen']]
-        with open(r'features.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(fields)
+        for result in r_page.json()['recordings']:
+            # Download features to csv for classification
+            fields = [result['id'], result['gen'], result['sp'], result['ssp'], result['en'], result['cnt'], result['loc'],
+                      result['type'], result['q'], result['length'], result['bird-seen'], result['file']]
+            if result['en'] == 'Grey Catbird':
+                print(result['id'])
+            with open(r'features.csv', 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(fields)
+print('downloaded all features')
